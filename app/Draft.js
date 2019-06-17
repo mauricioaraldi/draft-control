eba011d0a8ff8763af5785f1cd99050038572742
Mauricio Araldi <araldi.mauricio@gmail.com>
2019-06-16 21:09:32 -0300

    Refactoring server socket

--- app/Draft.js
+++ app/Draft.js
@@ -1,6 +1,7 @@
 import crypto from 'crypto';
 
 import DraftModel from './objects/DraftModel';
+import PlayerModel from './objects/PlayerModel';
 
 /**
  * Manages data and flows about drafts
@@ -79,6 +80,154 @@ export default class Draft {
 		}
 
 		Drafts[id].name = name;
+
+		return Drafts[id];
+	}
+
+	/**
+	 * Adds a player into the draft
+	 * 
+	 * @author mauricio.araldi
+	 * @since 0.8.0
+	 *
+	 * @param {Integer} id ID of the draft to have the player added
+	 * @param {String} name Name of the player to be added
+	 * @return {DraftModel} The draft from register
+	 */
+	static addPlayer(id, name) {
+		if (!Drafts[id].players) {
+			Drafts[id].players = {};
+		}
+
+		Drafts[id].players[name] = new PlayerModel(name, 0, 0, 0, 0, 0);
+
+		return Drafts[id];
+	}
+
+	/**
+	 * Builds the tournament table for a draft
+	 * 
+	 * @author mauricio.araldi
+	 * @since 0.8.0
+	 * 
+	 * @param {Integer} id ID of the draft to have its tournament table builded
+	 * @return {DraftModel} The draft from register
+	 */
+	static buildTournamentObject(id) {
+		const draft = Drafts[id];
+
+		if (!draft) {
+			throw Error(`No valid draft was found for the id ${id}`);
+		}
+
+		const tournament = {};
+
+		Object.values(draft.players).forEach(player => {
+			Object.values(draft.players).forEach(opponent => {
+				if (player.id === opponent.id) {
+					return;
+				}
+
+				if (!tournament[player.id]) {
+					tournament[player.id] = {};
+				}
+				
+				tournament[player.id][opponent.id] = { matchesWon: null, matchesLost: null };
+			});
+		});
+
+		Drafts[id].tournament = tournament;
+
+		return Drafts[id];
+	}
+
+	/**
+	 * Builds the suggested matches for a draft
+	 *
+	 * @author mauricio.araldi
+	 * @since 0.8.0
+	 *
+	 * @param {Integer} id ID of the draft to have its matches suggested
+	 * @return {DraftModel} The draft from register
+	 */
+	static buildSuggestedMatches(id) {
+		const draft = Drafts[id];
+
+		if (!draft) {
+			throw Error(`No valid draft was found for the id ${id}`);
+		}
+
+		const suggestedMatches = [],
+			alreadyEnrolled = [],
+			players = Object.values(draft.players);
+
+		//Sort players by number of games
+		players.sort((a, b) => { 
+			const aGames = a.gamesWon + a.gamesLost,
+				bGames = b.gamesWon + b.gamesLost;
+
+			return aGames - bGames;
+		});
+
+		players.forEach(player => {
+			//If a match was already suggested to this player, skip it
+			if (alreadyEnrolled.indexOf(player.id) > -1) {
+				return;
+			}
+
+			players.some(opponent => {
+				let matchesWon = null;
+
+				//If is the same as pĺayer, or if a match was already suggested for this opponent, skip it
+				if (player.id == opponent.id
+					|| alreadyEnrolled.indexOf(opponent.id) > -1) {
+					return;
+				}
+
+				matchesWon = parseInt(draft.tournament[player.id][opponent.id].matchesWon);
+
+				//If the player X opponent have played already, skip them
+				if (!isNaN(matchesWon)) {
+					return;
+				}
+
+				alreadyEnrolled.push(player.id);
+				alreadyEnrolled.push(opponent.id);
+
+				suggestedMatches.push(player.id +' x '+ opponent.id);
+
+				return true;
+			});
+		});
+
+		return suggestedMatches;
+	}
+
+	/**
+	 * Sets the score for a match
+	 *
+	 * @author mauricio.araldi
+	 * @since 0.8.0
+	 *
+	 * @param {Integer} id ID of the draft to have its matches suggested
+	 * @param {Integer} playerId The id of the player who played the match
+	 * @param {Integer} playerScore The score of the player who played the match
+	 * @param {Integer} opponentId The id of the opponent
+	 * @param {Integer} opponentScore The score of the opponent
+	 * @return {DraftModel} The draft from register
+	 */
+	static setMatchScore(id, playerId, playerScore, opponentId, opponentScore) {
+		if (!Drafts[id]) {
+			throw Error(`No valid draft was found for the id ${id}`);
+		}
+
+		//Updates table owner score
+		Drafts[id].tournament[playerId][opponentId]['matchesWon'] = playerScore ? parseInt(playerScore) : null;
+		Drafts[id].tournament[playerId][opponentId]['matchesLost'] = opponentScore ? parseInt(opponentScore) : null;
+		
+		//Updates opponent score
+		Drafts[id].tournament[opponentId][playerId]['matchesWon'] = opponentScore ? parseInt(opponentScore) : null;
+		Drafts[id].tournament[opponentId][playerId]['matchesLost'] = playerScore ? parseInt(playerScore) : null;
 
 		return Drafts[id];
 	}
--- app/sockets/server.js
+++ app/sockets/server.js
@@ -43,18 +43,16 @@ export default socket => {
 
 	/**
 	 * Sets the draft players
-	 * TODO
+	 * 
 	 * @author mauricio.araldi
-	 * @since 0.6.0
+	 * @since 0.8.0
 	 */
 	socket.on('players', data => {
-		Draft.players = {};
-
 		data.forEach(playerName => {
-			Draft.players[playerName] = new PlayerModel(playerName, 0, 0, 0, 0, 0);
+			draft = Draft.addPlayer(draft.id, playerName);
 		});
 
-		socket.emit('players', Draft.players);
+		socket.emit('players', draft.players);
 	});
 
 	/**
@@ -64,123 +62,33 @@ export default socket => {
 	 * @since  0.6.0
 	 */
 	socket.on('loadGame', data => {
-		socket.emit('loadGame', Draft);
-		socket.emit('suggestedMatches', buildSuggestedMatches());
+		// socket.emit('loadGame', Draft);
+		// socket.emit('suggestedMatches', buildSuggestedMatches());
 	});
 
 	/**
 	 * Builds the tournament table
-	 * TODO
+	 * 
 	 * @author mauricio.araldi
-	 * @since 0.6.0
+	 * @since 0.8.0
 	 */
 	socket.on('tournament', data => {
-		buildTournamentObject();
-		socket.emit('tournament', Draft.tournament);
-		socket.emit('suggestedMatches', buildSuggestedMatches());
+		draft = Draft.buildTournamentObject(draft.id);
+
+		socket.emit('tournament', draft.tournament);
+		socket.emit('suggestedMatches', Draft.buildSuggestedMatches(draft.id));
 	})
 
 	/**
 	 * Updates tournament scores
-	 * TODO
+	 * 
 	 * @author mauricio.araldi
-	 * @since 0.6.0
+	 * @since 0.8.0
 	 */
 	socket.on('updateScore', data => {
-		//Updates table owner score
-		Draft.tournament[data.playerId][data.opponentId]['matchesWon'] = data.playerScore ? parseInt(data.playerScore) : null;
-		Draft.tournament[data.playerId][data.opponentId]['matchesLost'] = data.opponentScore ? parseInt(data.opponentScore) : null;
-		
-		//Updates opponent score
-		Draft.tournament[data.opponentId][data.playerId]['matchesWon'] = data.opponentScore ? parseInt(data.opponentScore) : null;
-		Draft.tournament[data.opponentId][data.playerId]['matchesLost'] = data.playerScore ? parseInt(data.playerScore) : null;
+		draft = Draft.setMatchScore(draft.id, data.playerId, data.playerScore, data.opponentId, data.opponentScore);
 
-		socket.emit('tournament', Draft.tournament);
-		socket.emit('suggestedMatches', buildSuggestedMatches());
+		socket.emit('tournament', draft.tournament);
+		socket.emit('suggestedMatches', Draft.buildSuggestedMatches(draft.id));
 	});
 };
-
-
-
-/* --- Functions --- */ //TODO - Functions should not be in routing
-
-
-
-/**
- * Builds the tournament object
- *
- * @author mauricio.araldi
- * @since 0.6.0
- */
-function buildTournamentObject() {
-	Draft.tournament = {};
-
-	Object.values(Draft.players).forEach(player => {
-		Object.values(Draft.players).forEach(opponent => {
-			if (player.id == opponent.id) {
-				return;
-			}
-
-			if (!Draft.tournament[player.id]) {
-				Draft.tournament[player.id] = {};
-			}
-			
-			Draft.tournament[player.id][opponent.id] = {matchesWon:null, matchesLost:null};
-		});
-	});
-}
-
-/**
- * Builds the suggested matches
- *
- * @author mauricio.araldi
- * @since 0.6.0
- *
- * @return {Array} Array containing suggested matches
- */
-function buildSuggestedMatches() {
-	var suggestedMatches = [],
-		alreadyEnrolled = [],
-		players = Object.values(Draft.players);
-
-	//Sort players by number of games
-	players.sort((a, b) => { 
-		var aGames = a.gamesWon + a.gamesLost,
-			bGames = b.gamesWon + b.gamesLost;
-
-		return aGames - bGames;
-	});
-
-	players.forEach(player => {
-		//If a match was already suggested to this player, skip it
-		if (alreadyEnrolled.indexOf(player.id) > -1) {
-			return;
-		}
-
-		players.some(opponent => {
-			var matchesWon;
-
-			//If is the same as pĺayer, or if a match was already suggested for this opponent, skip it
-			if (player.id == opponent.id
-				|| alreadyEnrolled.indexOf(opponent.id) > -1) {
-				return;
-			}
-
-			matchesWon = parseInt(Draft.tournament[player.id][opponent.id].matchesWon);
-
-			//If the player X opponent have played already, skip them
-			if (!isNaN(matchesWon)) {
-				return;
-			}
-
-			alreadyEnrolled.push(player.id);
-			alreadyEnrolled.push(opponent.id);
-
-			suggestedMatches.push(player.id +' x '+ opponent.id);
-
-			return true;
-		});
-	});
-
-	return suggestedMatches;
-}
